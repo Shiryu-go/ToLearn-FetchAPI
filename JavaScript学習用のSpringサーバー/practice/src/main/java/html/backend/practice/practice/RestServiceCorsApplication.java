9dbf77144ae86609323a3358a501fb55743ed9f6
Shiryu Sasaki <ilovenhunhu777@gmail.com>
2023-08-23 23:53:49 +0900

    Fetch API でのデータの取得 DONE !!! ちょー疲れた

--- JavaScript学習用のSpringサーバー/practice/src/main/java/html/backend/practice/practice/RestServiceCorsApplication.java
+++ JavaScript学習用のSpringサーバー/practice/src/main/java/html/backend/practice/practice/RestServiceCorsApplication.java
@@ -7,18 +7,20 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PracticeApplication {
+public class RestServiceCorsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PracticeApplication.class, args);
+		SpringApplication.run(RestServiceCorsApplication.class, args);
 	}
-		@Bean
-		public WebMvcConfigurer corsConfigurer() {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://127.0.0.1:");
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
 			}
 		};
 	}
+
 }
